2fe1355d549529524daf3b1604e410db436c1473
ariw96 <ariw96@gmail.com>
2022-11-22 19:01:49 +0200

    build componets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import ProfileCard from "./components/ProfileCard";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>
+				<NavBar />
+			</h1>
+			<h2>
+				<Header />
+			</h2>
+			<h3>
+				<ProfileCard />
+			</h3>
+		</div>
+	);
 }
 
 export default App;
